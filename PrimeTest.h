5f2aae5a7449c7c39ef0ffbdfbc2ecb7da262eba
Alex Kreitzberg <ajkreitzberg@gmail.com>
2017-03-29 17:49:48 -0700

    Templated threading algorithm, works on GMP classes

--- PrimeTest.h
+++ PrimeTest.h
@@ -2,6 +2,12 @@
 #include<climits>
 #include<bitset>
 
+#pragma warning( push )
+#pragma warning( disable: 4146 )
+#include "gmp.h"
+#include "gmpxx.h"
+#pragma warning( pop )
+
 
 int mod(int a, int b);
 
@@ -15,3 +21,5 @@ int firstBitFromRight(std::bitset<numberIntSize> binary);
 bool MillerRabin(int power);
 
 bool isLikelyPrime(int workitem);
+
+bool isLikelyPrime(mpz_class workitem);
